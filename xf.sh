4955aefc23b74efd1fe40c73ba7e278a9a2d5314
xiexiaosi911 <54777144+xiexiaosi911@users.noreply.github.com>
2022-04-01 21:11:53 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "a161b93d-9731-4b6e-961b-921830163dcb",
                         "alterId": 0
                     }
                 ],
